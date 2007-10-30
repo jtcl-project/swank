7e751ebe9ede30ba4f787276132747280802cad6
Johnson Bruce <bruce@onemoonscientific.com>
2007-10-30 07:36:28 -0400

    Change startconsole to use menu rather than jmenu

--- src/com/onemoonscientific/swank/library/startconsole.tcl
+++ src/com/onemoonscientific/swank/library/startconsole.tcl
@@ -1,7 +1,5 @@
 console show
-jmenu .console.menubar.demos -tearoff 0 -label Demos
-  .console.menubar.demos add command -label "Widgets" -underline 2 \
-  -command "source [file join $tk_library demos widget]"
-.console.menubar jadd .console.menubar.demos
-#[.console object] pack
+menu .console.menubar.demos -tearoff 0 
+.console.menubar.demos add command -label "Widgets" -underline 2 -command "source [file join $tk_library demos widget]"
+.console.menubar add cascade  -label Demos -menu .console.menubar.demos
 vwait __done
