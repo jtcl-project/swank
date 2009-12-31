f65590af21a680b1b571b48926651fe31a7dfe99
Johnson Bruce <bruce@onemoonscientific.com>
2009-12-31 12:35:55 -0500

    Remove unused import

--- src/com/onemoonscientific/swank/canvas/SwkImageCanvas.java
+++ src/com/onemoonscientific/swank/canvas/SwkImageCanvas.java
@@ -25,7 +25,6 @@ import java.util.*;
 import java.util.Map.Entry;
 import javax.imageio.ImageIO;
 
-import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 /** Class for objects which represent a Swank swkcanvas widget. */
