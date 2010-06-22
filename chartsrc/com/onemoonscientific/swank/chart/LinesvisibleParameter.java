625e40696dec9a6054d51a8b7366d207f72f5857
Bruce Johnson <bruce@onemoonscientific.com>
2010-06-22 10:26:22 -0400

    Change Linesvisible and Shapesvisible to store their values in boolean[] field in case dataset hasn't been assigned yet.

--- chartsrc/com/onemoonscientific/swank/chart/LinesvisibleParameter.java
+++ chartsrc/com/onemoonscientific/swank/chart/LinesvisibleParameter.java
@@ -7,10 +7,15 @@ import com.onemoonscientific.swank.canvas.SwkShape;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import tcl.lang.Interp;
+import tcl.lang.TclException;
+import tcl.lang.TclList;
+import tcl.lang.TclObject;
+import tcl.lang.TclBoolean;
 
-class LinesvisibleParameter extends StringParameter {
-
-	private static String name = "linesvisible";
+class LinesvisibleParameter extends CanvasParameter {
+       boolean[] visible = new boolean[0];
+        private static final String name = "linesvisible";
 
 	LinesvisibleParameter() {
 		CanvasParameter.addParameter(this);
@@ -19,12 +24,28 @@ class LinesvisibleParameter extends StringParameter {
 	public String getName() {
 		return name;
 	}
+       public void setValue(Interp interp, SwkImageCanvas swkCanvas, TclObject arg) throws TclException {
+                TclObject[] visibleList = TclList.getElements(interp, arg);
+                if (visibleList.length == 0) {
+                        throw new TclException(interp, "bad visible value, must be \"value1 value2 ...\"");
+                }
+                boolean[] visibleTemp = new boolean[visibleList.length];
+                for (int i = 0; i < visibleList.length; i++) {
+                        visibleTemp[i] = TclBoolean.get(interp, visibleList[i]);
+                }
+                visible = visibleTemp;
+        }
 
-	public String getValue(SwkShape swkShape) {
-		XYPlot plot = ((XYPlotShape) swkShape).plot;
-		StringBuffer sBuf = new StringBuffer();
+	public TclObject getValue(Interp interp, SwkShape swkShape) throws TclException {
+		XYLineAndShapeComplete xyShape = (XYLineAndShapeComplete) swkShape;
+		XYPlot plot = xyShape.plot;
+                TclObject result = TclList.newInstance();
 		int nDatasets = plot.getDatasetCount();
 		int j = 0;
+                boolean[] linesVisible = xyShape.getLinesVisible();
+                for (boolean bValue:linesVisible) {
+                    TclList.append(interp,result,TclBoolean.newInstance(bValue));
+                }
 		for (int iData = 0; iData < nDatasets; iData++) {
 			XYItemRenderer renderer = ((XYPlotShape) swkShape).plot.getRenderer(iData);
 			if (renderer == null) {
@@ -32,53 +53,29 @@ class LinesvisibleParameter extends StringParameter {
 			}
 			if (renderer instanceof XYLineAndShapeRenderer) {
 				XYData data = (XYData) plot.getDataset(iData);
+                                if (data != null) {
 				int nSeries = data.getSeriesCount();
 				for (int i = 0; i < nSeries; i++) {
 					Boolean sVisible = ((XYLineAndShapeRenderer) renderer).getSeriesLinesVisible(i);
-					boolean bValue = false;
+					boolean bValue = true;
 					if (sVisible != null) {
 						bValue = sVisible.booleanValue();
 					}
-					if (i > 0) {
-						sBuf.append(' ');
-					}
-					if (bValue) {
-						sBuf.append('1');
-					} else {
-						sBuf.append('0');
-					}
+                                        TclList.append(interp,result,TclBoolean.newInstance(bValue));
 					j++;
 				}
+				}
 			} else {
-				sBuf.append('0');
+                                TclList.append(interp,result,TclBoolean.newInstance(false));
 			}
 		}
-		return sBuf.toString();
+		return result;
 	}
 
 	public void exec(SwkImageCanvas swkCanvas, SwkShape swkShape) {
-		XYPlot plot = ((XYPlotShape) swkShape).plot;
-		String newValue = getNewValue();
-		String[] values = newValue.split(" ");
-		int nDatasets = plot.getDatasetCount();
-		int j = 0;
-		for (int iData = 0; iData < nDatasets; iData++) {
-			XYItemRenderer renderer = ((XYPlotShape) swkShape).plot.getRenderer(iData);
-			if (renderer == null) {
-				renderer = ((XYPlotShape) swkShape).plot.getRenderer();
-			}
-			if (renderer instanceof XYLineAndShapeRenderer) {
-				XYData data = (XYData) plot.getDataset(iData);
-				int nSeries = data.getSeriesCount();
-				for (int i = 0; i < nSeries; i++) {
-					boolean bValue = true;
-					if (values.length > j) {
-						bValue = values[j].charAt(0) == '1';
-					}
-					((XYLineAndShapeRenderer) renderer).setSeriesLinesVisible(i, bValue);
-					j++;
-				}
-			}
-		}
+		if (swkShape instanceof XYLineAndShapeComplete)  {
+		    XYLineAndShapeComplete xyShape = (XYLineAndShapeComplete) swkShape;
+                    xyShape.setLinesVisible(visible);
+                }
 	}
 }
