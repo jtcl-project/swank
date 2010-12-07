3790cb30b1b175b90e035c04c1a70695d913f1d7
Bruce Johnson <bruce@onemoonscientific.com>
2010-12-06 20:28:25 -0500

    Remove unused imports in BellCmd

--- src/main/java/com/onemoonscientific/swank/BellCmd.java
+++ src/main/java/com/onemoonscientific/swank/BellCmd.java
@@ -28,10 +28,6 @@ import tcl.lang.*;
 import tcl.pkg.java.ReflectObject;
 import java.awt.*;
 
-import java.io.*;
-
-import java.util.*;
-
 public class BellCmd implements Command {
 
     public void cmdProc(Interp interp, TclObject[] argv)
